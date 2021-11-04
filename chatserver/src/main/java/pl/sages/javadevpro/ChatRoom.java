75dfdce1ff325b4dd951677e065736e43ebc0468
Anna Saweljew <anna.saweljew@o2.pl>
2021-11-04 13:02:36 +0100

    add chartroom message history

--- chatserver/src/main/java/pl/sages/javadevpro/ChatRoom.java
+++ chatserver/src/main/java/pl/sages/javadevpro/ChatRoom.java
@@ -1,14 +1,17 @@
 package pl.sages.javadevpro;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ChatRoom {
 
     private List<ChatUser> users = new ArrayList<>();
     private String chatRoomName;
+    private ConcurrentLinkedQueue<String> messageHistory = new ConcurrentLinkedQueue<>();
 
-    ///TODO HISTORY
 
     public ChatRoom(String name) {
         this.chatRoomName = name;
@@ -17,6 +20,7 @@ public class ChatRoom {
     public void addChatUser(ChatUser chatUser) {
         sendMessageToAllUsers("-- User: " + chatUser.getUserName() + " connected to chat room --", null);
         users.add(chatUser);
+        messageHistory.forEach(message ->  chatUser.writeMessage(message,null));
     }
 
     public void removeChatUser(ChatUser chatUser) {
@@ -30,7 +34,14 @@ public class ChatRoom {
 
     public void sendMessageToAllUsers(String message, String fromUser) {
         users.stream()
-            .filter(chatUser -> !chatUser.getUserName().equals(fromUser))
-            .forEach(chatUser -> chatUser.writeMessage(message, fromUser));
+                .filter(chatUser -> !chatUser.getUserName().equals(fromUser))
+                .forEach(chatUser -> chatUser.writeMessage(message, fromUser));
+        addMessageToHistory(message);
     }
+
+    public void addMessageToHistory(String message) {
+        messageHistory.removeIf(x -> messageHistory.size()>=10);
+        messageHistory.add(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:s")) + message);
+    }
+
 }
